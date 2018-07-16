9ac88822f26d5ac7d02b6ebfc89c9f06810c12d5
Timo <30553356+y21@users.noreply.github.com>
2018-07-16 20:52:18 +0200

    Don't >0 if br >9999

--- algrithms/basic.cc
+++ algrithms/basic.cc
@@ -1,11 +1,11 @@
-#include <iostream>
+a#include <iostream>
 #include <cmath>
 #include <string>
 
 int fight(unsigned int own_br, unsigned int br_of_opponent, bool won)
 {
 	int gainValue = 0;
-	int br_difference = own_br - br_of_opponent; 
+	int br_difference = own_br - br_of_opponent;
 	if (won)
 	{
 		if (br_difference < 0)
@@ -19,7 +19,7 @@ int fight(unsigned int own_br, unsigned int br_of_opponent, bool won)
 	}
 	else
 	{
-		if(br_difference > 0)
+		if (br_difference > 0)
 		{
 			gainValue = -gamma(sqrt(fdim(own_br, br_of_opponent)));
 		}
@@ -29,26 +29,31 @@ int fight(unsigned int own_br, unsigned int br_of_opponent, bool won)
 		}
 	}
 
+	if (own_br + gainValue >= 9999)
+	{
+		return 0;
+	}
+
 	return gainValue == 0 ? ++gainValue : gainValue;
 }
 
 int main()
 {
-	while(1)
+	while (1)
 	{
-	unsigned int own_br;
-	unsigned int br_of_opponent;
-	bool won;
-	std::string input;
-	std::cout << "Own BR: ";
-	std::cin >> own_br;
-	std::cout << "BR of opponent: ";
-	std::cin >> br_of_opponent;
-	std::cout << "Won? (y/n)";
-	std::cin >> input;
-	won = input == "y";
+		unsigned int own_br;
+		unsigned int br_of_opponent;
+		bool won;
+		std::string input;
+		std::cout << "Own BR: ";
+		std::cin >> own_br;
+		std::cout << "BR of opponent: ";
+		std::cin >> br_of_opponent;
+		std::cout << "Won? (y/n)";
+		std::cin >> input;
+		won = input == "y";
 
-	std::cout << fight(own_br, br_of_opponent, won) << "\n\n";
+		std::cout << fight(own_br, br_of_opponent, won) << "\n\n";
 	}
 	int a;
 	std::cin >> a;
