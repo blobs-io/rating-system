f971d30a6490a5ce53a5de58c125b9936ff39df7
Timo <30553356+y21@users.noreply.github.com>
2018-12-07 21:46:31 +0100

    Fix bad calculations and use old one

--- algrithms/blobrating.cc
+++ algrithms/blobrating.cc
@@ -1,13 +1,20 @@
 int calc(unsigned int own_br, unsigned int br_of_opponent)
 {
 	int gainValue = 0;
-	
-	if (br_of_opponent > own_br) {
-		gainValue = lgamma(sqrt(fdim(br_of_opponent, own_br)));
-	} else {
-		gainValue = log1p(9998 / fdim(own_br, br_of_opponent)) * 12.5;
+	int br_difference = own_br - br_of_opponent;
+	if (br_difference < 0)
+	{
+		gainValue = pow(log10(-br_difference), 4);
 	}
-	
-	
+	else
+	{
+		gainValue = log1p(9998 / fdim(own_br, br_of_opponent));
+	}
+
+	if (own_br + gainValue >= 9999)
+	{
+		return 0;
+	}
+
 	return gainValue == 0 ? ++gainValue : gainValue;
 }
