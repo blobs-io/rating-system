e711c1eb37ec9793d9eae24572d322a2e89330cc
Timo <30553356+y21@users.noreply.github.com>
2019-11-10 11:36:52 +0100

    Create algorithm.cc

---
+++ algorithm.cc
@@ -0,0 +1,14 @@
+short calc(unsigned int own_br, unsigned int br_of_opponent) {
+    int gainValue = 0;
+    int br_difference = own_br - br_of_opponent;
+    if (br_difference < 0) {
+        gainValue = pow(log10(-br_difference), 4);
+    } else {
+        gainValue = log1p(9998 / fdim(own_br, br_of_opponent));
+    }
+    if (own_br + gainValue >= 9999) {
+        return 0;
+    }
+    if (gainValue > 999 || gainValue < 0) return 32;
+    return gainValue == 0 ? ++gainValue : gainValue;
+}
